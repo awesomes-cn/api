98c953389b1d8714bc8b3eedd8ef2f2ca93fc7e7
hxh <1246996371@qq.com>
2017-07-15 20:59:42 -0700

    reset tesk config

--- task/build.js
+++ task/build.js
@@ -1,11 +1,11 @@
 // 创建定时任务到系统
 
-// require('crontab').load((err, crontab) => {
-//   crontab.reset()
-//   crontab.create(`node ${__dirname}/sync_repo_attr.js`, '0 1 * * *')
-//   crontab.create(`node ${__dirname}/sync_repo_trend.js`, '0 5 * * *')
-//   crontab.create(`node ${__dirname}/fetch_mem_avatar.js`, '0 5 * * *')
-//   crontab.save((err, crontab) => {
-//     console.log('写入定时任务成功')
-//   })
-// })
+require('crontab').load((err, crontab) => {
+  crontab.reset()
+  crontab.create(`node ${__dirname}/sync_repo_attr.js`, '0 1 * * *')
+  crontab.create(`node ${__dirname}/sync_repo_trend.js`, '0 5 * * *')
+  crontab.create(`node ${__dirname}/fetch_mem_avatar.js`, '0 3 * * *')
+  crontab.save((err, crontab) => {
+    console.log('写入定时任务成功')
+  })
+})
