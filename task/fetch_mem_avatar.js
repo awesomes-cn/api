f7f6fbc09be43c1672a635c41ca3b9b5d6147b4a
hxh <1246996371@qq.com>
2017-07-16 19:14:47 -0700

    change fetch mem avatar task

--- task/fetch_mem_avatar.js
+++ task/fetch_mem_avatar.js
@@ -3,24 +3,30 @@
 const Mem = require('../models/mem')
 const Aliyun = require('../lib/aliyun')
 
-let fetchAvatars = async () => {
-  let mems = await Mem.fetchAll()
-  for (let mem of mems) {
-    await fetchOne(mem)
-  }
-}
-
 let fetchOne = mem => {
   return new Promise(resolve => {
+    if (!/^http[s]?:\/\//.test(mem.avatar)) {
+      resolve()
+      return
+    }
     setTimeout(async () => {
       let _filename = `${Date.now()}-${mem.id}-${parseInt(Math.random() * 10000)}.png`
-      await Aliyun.uploadUrl(mem.get('avatar'), `mem/${_filename}`)
-      mem.set('avatar', _filename)
-      await mem.save()
+      console.log(mem.avatar, `mem/${_filename}`)
+      await Aliyun.uploadUrl(mem.avatar, `mem/${_filename}`)
+      mem.avatar = _filename
+      await Mem.forge(mem).save()
       console.log(`同步${mem.id}头像成功：${_filename}`)
       resolve()
     }, 1000)
   })
+}
+
+let fetchAvatars = async () => {
+  let mems = await Mem.fetchAll()
+  mems = mems.toJSON()
+  for (let mem of mems) {
+    await fetchOne(mem)
+  }
 }
 
 fetchAvatars()
