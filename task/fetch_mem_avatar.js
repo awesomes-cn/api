1869bdaf6bdcb547112480ec6b1f8879184cc79a
hxh <1246996371@qq.com>
2017-07-16 18:59:22 -0700

    change fetch mem avatar task

--- task/fetch_mem_avatar.js
+++ task/fetch_mem_avatar.js
@@ -3,25 +3,25 @@
 const Mem = require('../models/mem')
 const Aliyun = require('../lib/aliyun')
 
-let memAvatarTask = id => {
-  Mem.where('id', '>', id).fetch().then(async mem => {
-    if (mem) {
-      if (/^http[s]?:\/\//.test(mem.get('avatar'))) {
-        await new Promise(resolve => {
-          setTimeout(async () => {
-            let _filename = `${Date.now()}-${mem.id}-${parseInt(Math.random() * 10000)}.png`
-            await Aliyun.uploadUrl(mem.get('avatar'), `mem/${_filename}`)
-            mem.set('avatar', _filename)
-            await mem.save()
-            console.log(`同步${mem.id}头像成功：${_filename}`)
-            resolve()
-          }, 1000)
-        })
-      }
-    }
-    memAvatarTask(mem.id)
+let fetchAvatars = async () => {
+  let mems = await Mem.fetchAll()
+  for (let mem of mems) {
+    await fetchOne(mem)
+  }
+}
+
+let fetchOne = mem => {
+  return new Promise(resolve => {
+    setTimeout(async () => {
+      let _filename = `${Date.now()}-${mem.id}-${parseInt(Math.random() * 10000)}.png`
+      await Aliyun.uploadUrl(mem.get('avatar'), `mem/${_filename}`)
+      mem.set('avatar', _filename)
+      await mem.save()
+      console.log(`同步${mem.id}头像成功：${_filename}`)
+      resolve()
+    }, 1000)
   })
 }
 
-memAvatarTask(0)
+fetchAvatars()
 
