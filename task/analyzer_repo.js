2556da7ef6908fcc07a56a01ce155d617ea8a6be
hxh <1246996371@qq.com>
2017-09-12 05:21:51 -0700

    slit analyze repo task

--- task/analyzer_repo.js
+++ task/analyzer_repo.js
@@ -3,13 +3,22 @@ const Repo = require('../models/repo')
 
 let action = async (repoID) => {
   let _repo = await Repo.where('id', '>', repoID).fetch()
-  try {
-    let score = await analyzer(_repo.toJSON())
-    _repo.set('score', score)
-    await _repo.save()
-  } catch (ex) {
-    console.log(' [x] 分析失败')
+  if (!_repo) {
+    return
   }
+
+  // 偶数
+  let day = new Date().getDate()
+  if ((_repo.id - day) % 2 === 0) {
+    try {
+      let score = await analyzer(_repo.toJSON())
+      _repo.set('score', score)
+      await _repo.save()
+    } catch (ex) {
+      console.log(' [x] 分析失败')
+    }
+  }
+
   setTimeout(() => {
     action(_repo.id)
   }, 3000)
