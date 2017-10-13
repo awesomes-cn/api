7f3eb8c8381db56ed89cfbc825dc3c0e1e385c19
hxh <1246996371@qq.com>
2017-10-12 18:23:42 -0700

    sync repo attr task

--- task/sync_repo_attr.js
+++ task/sync_repo_attr.js
@@ -6,8 +6,12 @@ const RepoHelper = require('../lib/repo')
 let syncRepoTask = id => {
   Repo.where('id', '>', id).fetch().then(async item => {
     if (item) {
-      await RepoHelper.sync(item)
-      console.log(`${item.get('full_name')} 同步成功`)
+      try {
+        await RepoHelper.sync(item)
+        console.log(`${item.get('full_name')} 同步成功`)
+      } catch (ex) {
+        console.log(' [x]同步出错了', ex)
+      }
       setTimeout(() => {
         syncRepoTask(item.id)
       }, 1200)
