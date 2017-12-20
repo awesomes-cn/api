16766b84c102302bc32250f95c05a8fa26fbbddd
hxh <1246996371@qq.com>
2017-12-19 18:48:05 -0800

    more feex feature

--- controllers/feex.js
+++ controllers/feex.js
@@ -34,15 +34,15 @@ module.exports = {
 
   get_index_id: async (req, res) => {
     let _related = {}
-   if (req.query.relation) {
-    _related = {
-      withRelated: ['catalogs', {
-        mem: query => {
-          query.select('id', 'nc', 'avatar')
-        }
-      }]
+    if (req.query.relation) {
+      _related = {
+        withRelated: ['catalogs', {
+          mem: query => {
+            query.select('id', 'nc', 'avatar')
+          }
+        }]
+      }
     }
-   }
 
     let item = await Feex.where({
       id: req.params.action
@@ -91,7 +91,7 @@ module.exports = {
       item: _feex
     })
   },
-  
+
   get_catalog: async (req, res) => {
     let items = await FeexCatalog.where({
       feex_id: req.params.id
@@ -101,12 +101,114 @@ module.exports = {
     })
   },
 
+  post_catalog: async (req, res) => {
+    if (!isMyFeex(res, req.params.id)) {
+      res.sendStatus(401)
+      res.send({status: false})
+      return
+    }
+    let item = {
+      'feex_id': req.params.id
+    }
+    ;['title', 'feex_file_id', 'isfree', 'parent'].forEach(key => {
+      if (req.body[key] !== undefined) {
+        item[key] = req.body[key]
+      }
+    })
+    // 排序
+    let _maxorderItem = await FeexCatalog.where({
+      'feex_id': req.params.id
+    }).query({
+      orderByRaw: 'ordernum desc',
+      limit: 1
+    }).fetch()
+    let _order = _maxorderItem ? (_maxorderItem.get('ordernum') + 1) : 1
+    item.ordernum = _order
+    let resitem = await FeexCatalog.forge(item).save()
+    res.send({
+      status: true,
+      item: resitem
+    })
+  },
+
+  put_catalog: async (req, res) => {
+    let _feexst = await FeexCatalog.where({
+      id: req.body.id
+    }).fetch()
+
+    if (!_feexst) {
+      res.sendStatus(404)
+      res.send('404')
+      return
+    }
+
+    if (!isMyFeex(res, _feexst.feex_id)) {
+      res.sendStatus(401)
+      res.send({status: false})
+      return
+    }
+
+    let item = {}
+    ;['id', 'title', 'feex_file_id', 'isfree', 'parent'].forEach(key => {
+      if (req.body[key] !== undefined) {
+        item[key] = req.body[key]
+      }
+    })
+    let resitem = await FeexCatalog.forge(item).save()
+    res.send({
+      status: true,
+      item: resitem
+    })
+  },
+
+  delete_catalog: async (req, res) => {
+    let item = await FeexCatalog.where({
+      id: req.query.id
+    }).fetch()
+    let children = await FeexCatalog.where({
+      parent: req.query.id
+    }).fetchAll()
+    children.push(item)
+    await Promise.all(children.map(item => {
+      item.destroy()
+    }))
+    res.send({
+      status: true
+    })
+  },
+
+  put_catalog_sort: async (req, res) => {
+    let dist = req.body.dist
+    let cates = await FeexCatalog.where('id', 'in', dist).fetchAll()
+    cates = cates.toJSON()
+    // 验证
+    let tmp = cates[0].ordernum
+    cates[0].ordernum = cates[1].ordernum
+    cates[1].ordernum = tmp
+    await FeexCatalog.forge(cates[0]).save()
+    await FeexCatalog.forge(cates[1]).save()
+    res.send({
+      status: true
+    })
+  },
+
   get_structure: async (req, res) => {
     let items = await FeexStructure.where({
       feex_id: req.params.id
+    }).query({
+      select: ['id', 'feex_id', 'name', 'type', 'file_from', 'parent']
     }).fetchAll()
     res.send({
       items: items
+    })
+  },
+
+  get_structure_con: async (req, res) => {
+    let st = await FeexStructure.where({
+      id: req.query.id
+    }).fetch()
+    res.send({
+      con: st.get('file_con')
     })
   },
 
--- migrations/20171208111934_create_table_feex_structures.js
+++ migrations/20171208111934_create_table_feex_structures.js
@@ -7,7 +7,7 @@ exports.up = function (knex, Promise) {
     table.string('type') // 文件 file  文件夹  folder
     table.string('file_from') // 文件来源 数据库 file   上传 upload
     table.string('file_upload') // 如果是上传的话，则给定上传文件名
-    table.integer('feex_file_id') // 如果是来源于 feex_file 数据库的话，则给定ID
+    table.text('file_con') // 文件内容
     table.integer('parent').defaultTo(0)
     table.timestamps()
   })
